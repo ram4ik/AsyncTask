a242ac8021e2e5a9708d0d8bf3cbccdc58fd6d48
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-11-04 16:40:59 +0200

    Initial Commit

---
+++ AsyncTask/AsyncTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncTaskApp.swift
+//  AsyncTask
+//
+//  Created by ramil on 04.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct AsyncTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
